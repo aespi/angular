65b0c63555fc30a6e83dcc7eec2e6e7443fc546b
SP <Simranpal Singh>
2024-06-26 16:33:14 +0530

    recom tabs dded

--- src/app/prod-desc/prod-desc.component.ts
+++ src/app/prod-desc/prod-desc.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+interface recomChips {
+  color?: string;
+  text: string;
+}
 @Component({
   selector: 'app-prod-desc',
   templateUrl: './prod-desc.component.html',
   styleUrls: ['./prod-desc.component.scss']
 })
-export class ProdDescComponent {
+export class ProdDescComponent implements OnInit {
+  recData: any[] = [];
+  selectedRed: recomChips[] = [];
+  selectedRedIdx: number = 0;
   productData = {
     thumbnails: ['../../assets/shoe1.png', '../../assets/shoe2.png', '../../assets/shoe3.png'],
     title: 'Black + leather + tops',
@@ -30,7 +36,87 @@ export class ProdDescComponent {
     },
     priceRange: { min: 120, max: 150, start: 0, end: 500 },
     tags: ['Alpha', 'Beta', 'Theta'],
-    recommendedComponent: { component: 'Material' }
+    recommendedComponent: [{ component: 'Upper', Material: 'Full Grain Leather, Mesh inserts' }],
+    chips: [{ color: [], text: '' }]
   };
   selected = { thumb: this.productData.thumbnails[0], tag: this.productData.tags[0] };
+
+  selectRecom(recom: any): void {
+    this.selectedRed = [];
+    // {
+    //   Recommendation: 'Recommendation 1',
+    //   Material: ['suede'],
+    //   'Closure Type': ['Buckle'],
+    //   'Support Type': ['Speed'],
+    //   Color: ['Maroon'],
+    //   'Sole Width': ['Wide'],
+    //   Sustainability: ['Not available'],
+    //   Athleisure: ['Not available'],
+    //   'Customization Option': ['Not available'],
+    //   'Tech Enhanced': ['Not available'],
+    //   Vintage: ['Not available']
+    // }
+    const selected = { ...recom };
+    delete selected['Recommendation'];
+    Object.keys(selected).forEach(r => {
+      if (selected[r].length) {
+        selected[r].forEach((c: any) => {
+          if (r === 'Color') {
+            this.selectedRed.push({ color: c, text: c });
+          } else if (c !== 'Not available') {
+            this.selectedRed.push({ text: c });
+          }
+        });
+      }
+    });
+  }
+
+  getRecomData(): void {
+    //YOUR API HERE...
+    this.recData = [
+      {
+        Recommendation: 'Recommendation 1',
+        Material: ['suede'],
+        'Closure Type': ['Buckle'],
+        'Support Type': ['Speed'],
+        Color: ['Maroon'],
+        'Sole Width': ['Wide'],
+        Sustainability: ['Not available'],
+        Athleisure: ['Not available'],
+        'Customization Option': ['Not available'],
+        'Tech Enhanced': ['Not available'],
+        Vintage: ['Not available']
+      },
+      {
+        Recommendation: 'Recommendation 2',
+        Material: ['Leather'],
+        'Closure Type': ['Lace'],
+        'Support Type': ['Stability'],
+        Color: ['Purple'],
+        'Sole Width': ['Wide'],
+        Sustainability: ['Not available'],
+        Athleisure: ['Not available'],
+        'Customization Option': ['Not available'],
+        'Tech Enhanced': ['Not available'],
+        Vintage: ['Not available']
+      },
+      {
+        Recommendation: 'Recommendation 3',
+        Material: ['Leather'],
+        'Closure Type': ['Hook and Loop'],
+        'Support Type': ['Stability'],
+        Color: ['Purple'],
+        'Sole Width': ['Wide'],
+        Sustainability: ['Not available'],
+        Athleisure: ['Not available'],
+        'Customization Option': ['Not available'],
+        'Tech Enhanced': ['Not available'],
+        Vintage: ['Not available']
+      }
+    ];
+    this.selectRecom(this.recData[this.selectedRedIdx]);
+  }
+  ngOnInit(): void {
+    this.getRecomData();
+  }
 }
